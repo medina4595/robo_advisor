076eda676d77f4ea7cf0fd44dc606287c40a9930
Jose Medina <medina.jose003@gmail.com>
2022-03-29 09:17:40 -0500

    Update lambda_function.py

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,8 +1,6 @@
-### Required Libraries ###
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-### Functionality Helper Functions ###
 def parse_int(n):
     """
     Securely converts a non-integer value to integer.
@@ -27,7 +25,6 @@ def build_validation_result(is_valid, violated_slot, message_content):
     }
 
 
-### Dialog Actions Helper Functions ###
 def get_slots(intent_request):
     """
     Fetch all the slots and their values from the current intent.
@@ -79,40 +76,6 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
-
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
-
-
-### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
     Performs dialog management and fulfillment for recommending a portfolio.
@@ -124,9 +87,6 @@ def recommend_portfolio(intent_request):
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]    
 
-
-
-### Intents Dispatcher ###
 def dispatch(intent_request):
     """
     Called when the user specifies an intent for this bot.
@@ -140,8 +100,6 @@ def dispatch(intent_request):
 
     raise Exception("Intent with name " + intent_name + " not supported")
 
-
-### Main Handler ###
 def lambda_handler(event, context):
     """
     Route the incoming request based on intent.
